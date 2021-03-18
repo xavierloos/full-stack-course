2989a8115a893ade7ec8c07094c72fe911354ac4
xavierloos <xavier.loos04@gmail.com>
2021-03-18 16:48:21 +0000

    5 - We need to generate a random number between 0 and 7

--- magic-8-ball.js
+++ magic-8-ball.js
@@ -3,3 +3,5 @@ userName ? console.log(`Hello, ${userName}`) : console.log(" Hello ")
 
 const userQuestion = "Will I become a werewolf tonight? ";
 console.log(userQuestion)
+
+let randomNumber = Math.floor(Math.random() * 8)
