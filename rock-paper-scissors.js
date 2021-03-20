ce0f8301be781ac1034bf1754dc05c1a4f96ea22
xavierloos <xavier.loos04@gmail.com>
2021-03-20 13:54:17 +0000

    Step 6

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,6 +3,7 @@
 // 3. When getting the user’s choice, you should also check to make sure that the user typed a valid choice: ‘rock’, ‘paper’, or ‘scissors’.
 // 4. Test the function by calling it with valid and invalid input, and printing the results to the console. You can delete this when you know your function works.
 // 5. Now we need to have the computer make a choice.
+// 6. Test the function by calling it multiple times and printing the results to the console. You can delete this when you know your function works.
 const getUserChoice = userInput =>{
   userInput = userInput.toLowerCase();
   if(userInput==='rock' || userInput==='paper'|| userInput==='scissors'){
