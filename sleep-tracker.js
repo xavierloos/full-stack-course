b51ce0c1aa24a310c80e44a1f26499167c8360b4
xavierloos <xavier.loos04@gmail.com>
2021-03-22 12:25:48 +0000

    Step 3

--- sleep-tracker.js
+++ sleep-tracker.js
@@ -1,5 +1,6 @@
 // 1. The first problem to solve is determining how many hours of sleep you got each night of the week. You can create a function that returns any given night’s number of hours of rest. Instead of writing seven different functions (one for each day of the week), let’s write one function with a parameter for the day.
 // 2.The function should accept a day as an argument and return the number of hours you slept that night. For instance, if you got 8 hours of sleep on Monday night, calling getSleepHours('monday') should return 8. Use an if / else or switch statement to implement this.
+// 3. Test the function by calling it multiple times and printing the results to the console. You can remove the tests when you know you function works.
 const getSleepHours = day => {
   switch (day) {
     case 'monday':
