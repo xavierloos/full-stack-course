9285e54a224539cadfd62e0a1a8408deebf8910e
xavierloos <xavier.loos04@gmail.com>
2021-03-22 12:29:20 +0000

    Step 4

--- sleep-tracker.js
+++ sleep-tracker.js
@@ -1,6 +1,10 @@
 // 1. The first problem to solve is determining how many hours of sleep you got each night of the week. You can create a function that returns any given night’s number of hours of rest. Instead of writing seven different functions (one for each day of the week), let’s write one function with a parameter for the day.
 // 2.The function should accept a day as an argument and return the number of hours you slept that night. For instance, if you got 8 hours of sleep on Monday night, calling getSleepHours('monday') should return 8. Use an if / else or switch statement to implement this.
 // 3. Test the function by calling it multiple times and printing the results to the console. You can remove the tests when you know you function works.
+// 4. Now that you’ ve written a function to get the sleep hours for each night, we need to do three things:
+// - Get the total sleep hours that you actually slept
+// - Get the ideal sleep hours that you prefer
+// - Calculate the sleep debt, if any.
 const getSleepHours = day => {
   switch (day) {
     case 'monday':
@@ -25,4 +29,7 @@ const getSleepHours = day => {
       return 8
       break
   }
+}
+const getActualSleepHours = () => {
+
 }
