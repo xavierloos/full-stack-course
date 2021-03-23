9729fef39bcb061c1cad826db821e233aeed5a84
xavierloos <xavier.loos04@gmail.com>
2021-03-23 20:51:43 +0000

    Step 4

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -6,3 +6,9 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
   return Math.floor(Math.random() * 10);
 }
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
