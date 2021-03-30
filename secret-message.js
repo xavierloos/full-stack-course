97eccd9975d4e227fafe632fa38379d08e11da95
xavierloos <xavier.loos04@gmail.com>
2021-03-30 11:32:29 +0100

    Step 2

--- secret-message.js
+++ secret-message.js
@@ -2,3 +2,5 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 // 1.  Use an array method to remove the last string of the array secretMessage.
 secretMessage.pop()
+// 2. Great! You can check your work by logging the .length of the array.
+console.log(secretMessage.length)
