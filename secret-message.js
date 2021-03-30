9db8285e8b0eaf08eeb2e9f34efee810a4190972
xavierloos <xavier.loos04@gmail.com>
2021-03-30 11:28:48 +0100

    Set up

---
+++ secret-message.js
@@ -0,0 +1,2 @@
+// Using array methods, you will transform an array of strings into a secret message!
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
