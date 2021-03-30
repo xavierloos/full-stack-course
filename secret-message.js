aee51a89781d5d573a633d72c18c099f75041412
xavierloos <xavier.loos04@gmail.com>
2021-03-30 11:30:05 +0100

    Step 1

--- secret-message.js
+++ secret-message.js
@@ -1,2 +1,4 @@
 // Using array methods, you will transform an array of strings into a secret message!
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+// 1.  Use an array method to remove the last string of the array secretMessage.
+secretMessage.pop()
